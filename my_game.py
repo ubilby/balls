5dcd215d0d34d8a48db5e00e645d1e32693545e4
ubilby <ubilby@gmail.com>
2020-12-07 21:01:37 +0300

    добавил спрайты врагов

--- my_game.py
+++ my_game.py
@@ -15,6 +15,26 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
 
+# создаем класс врагов
+class Mob(pygame.sprite.Sprite):
+    def __init__(self):
+        # инициализируем спрайт, задаем его параметры
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((30, 40))
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(WIDTH - self.rect.width)
+        self.rect.y = random.randrange(-100, -40)
+        self.speedy = random.randrange(1, 8)
+
+    def update(self):
+        self.rect.y += self.speedy
+        if self.rect.top > HEIGHT + 10:
+            self.rect.x = random.randrange(WIDTH - self.rect.width)
+            self.rect.y = random.randrange(-100, -40)
+            self.speedy = random.randrange(1, 8)
+
+
 # наследуемся от pygame.sprite
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -60,6 +80,13 @@ all_sprites = pygame.sprite.Group()
 player = Player()
 # добавляем спрайт в группу
 all_sprites.add(player)
+# тоже самое с мобами
+mobs = pygame.sprite.Group()
+for i in range(8):
+    m = Mob()
+    all_sprites.add(m)
+    mobs.add(m)
+
 
 # Цикл игры
 running = True
